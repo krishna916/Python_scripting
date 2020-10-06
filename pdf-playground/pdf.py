ce49fbdc7d92a5eacdaa74880a40db15a7424bd1
krishnamurti <krishna916@outlook.com>
2020-10-06 08:39:14 +0530

    merge watermark

--- pdf-playground/pdf.py
+++ pdf-playground/pdf.py
@@ -1,7 +1,22 @@
 import PyPDF2
 import sys
 
-inputs = sys.argv[1:]
+
+
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
+
+# inputs = sys.argv[1:]
 
 # with open('dummy.pdf', 'rb') as file:
 #     reader = PyPDF2.PdfFileReader(file)
@@ -12,11 +27,11 @@ inputs = sys.argv[1:]
 #     with open('tilt.pdf', 'wb') as new_file:
 #         writer.write(new_file)
 
-def pdf_combiner(pdf_list):
-    merger = PyPDF2.PdfFileMerger()
-    for pdf in pdf_list:
-        print(pdf)
-        merger.append(pdf)
-    merger.write('super.pdf')
+# def pdf_combiner(pdf_list):
+#     merger = PyPDF2.PdfFileMerger()
+#     for pdf in pdf_list:
+#         print(pdf)
+#         merger.append(pdf)
+#     merger.write('super.pdf')
 
-pdf_combiner(inputs)
+# pdf_combiner(inputs)
